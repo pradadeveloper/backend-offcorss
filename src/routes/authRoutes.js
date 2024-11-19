8f3404bcdafc656220945fbc6d75226329ffe8c0
unknown <juanfelipepradaing@gmail.com>
2024-11-18 19:05:47 -0500

    Reemplazando bcrypt por bcryptjs

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/clienteModel');
 
 // Endpoint de autenticación
